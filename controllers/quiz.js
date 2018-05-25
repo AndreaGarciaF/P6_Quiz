fe2c6d54aad7a5f245c359d143e4274c95924918
Andrea García Fernández <andrea.garciaf@alumnos.upm.es>
2018-05-25 13:16:32 +0200

    practica6 v.1

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -178,7 +178,7 @@ exports.randomplay = (req, res, next) => {
             where: whereOpt,
             offset: Math.floor(Math.random() * count),
             limit: 1
-        })
+        });
         //let id = Math.floor(Math.random()*quizzes.length);
         //let quiz = quizzes[id];
     })
@@ -216,7 +216,7 @@ exports.randomcheck = (req, res, next) => {
         req.session.randomPlay = req.session.randomPlay.concat(quiz.id);
     } else {
         req.session.randomPlay = [];
-    };
+    }
     res.render('quizzes/random_result', {
         result,
         answer,
